4b9316731903c38932edbf7675edf7cc77ac850a
Guilherme Campos <guiicampos31@gmail.com>
2025-03-29 17:41:05 -0300

    model criado, sem permanencia de dados

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("COmpensa slc");    
+
+});
+
 app.listen(3000, () => {
     console.log("Servidor Funcionando");
 });
